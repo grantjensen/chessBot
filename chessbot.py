d524a3cc49aad837d481b5aefc8b0602ca5dfacb
Grant Jensen <gjensen6@wisc.edu>
2021-08-09 19:38:41 -0500

    Remaining upgrades are passing prevdict through minimax and manually updating along with iterative deepening combined w transposition tables

--- chessbot.py
+++ chessbot.py
@@ -2,65 +2,67 @@ import chess
 import random
 import chess.syzygy
 import numpy as np
+import sys
+import dask
 
-pawn=np.array([[ 0,  0,  0,  0,  0,  0,  0,  0],
-[50, 50, 50, 50, 50, 50, 50, 50],
-[10, 10, 20, 30, 30, 20, 10, 10],
-[5,  5, 10, 25, 25, 10,  5,  5],
-[0,  0,  0, 20, 20,  0,  0,  0],
-[5, -5,-10,  0,  0,-10, -5,  5],
-[5, 10, 10,-20,-20, 10, 10,  5],
-[0,  0,  0,  0,  0,  0,  0,  0]])
+pawn=np.array([0,  0,  0,  0,  0,  0,  0,  0,
+50, 50, 50, 50, 50, 50, 50, 50,
+10, 10, 20, 30, 30, 20, 10, 10,
+ 5,  5, 10, 25, 25, 10,  5,  5,
+ 0,  0,  0, 20, 20,  0,  0,  0,
+ 5, -5,-10,  0,  0,-10, -5,  5,
+ 5, 10, 10,-20,-20, 10, 10,  5,
+ 0,  0,  0,  0,  0,  0,  0,  0])
 pawn+=100
 
-knight=np.array([[-50,-40,-30,-30,-30,-30,-40,-50],
-[-40,-20,  0,  0,  0,  0,-20,-40],
-[-30,  0, 10, 15, 15, 10,  0,-30],
-[-30,  5, 15, 20, 20, 15,  5,-30],
-[-30,  0, 15, 20, 20, 15,  0,-30],
-[-30,  5, 10, 15, 15, 10,  5,-30],
-[-40,-20,  0,  5,  5,  0,-20,-40],
-[-50,-40,-30,-30,-30,-30,-40,-50]])
+knight=np.array([-50,-40,-30,-30,-30,-30,-40,-50,
+-40,-20,  0,  0,  0,  0,-20,-40,
+-30,  0, 10, 15, 15, 10,  0,-30,
+-30,  5, 15, 20, 20, 15,  5,-30,
+-30,  0, 15, 20, 20, 15,  0,-30,
+-30,  5, 10, 15, 15, 10,  5,-30,
+-40,-20,  0,  5,  5,  0,-20,-40,
+-50,-40,-30,-30,-30,-30,-40,-50])
 knight+=305
 
-bishop=np.array([[-20,-10,-10,-10,-10,-10,-10,-20],
-[-10,  0,  0,  0,  0,  0,  0,-10],
-[-10,  0,  5, 10, 10,  5,  0,-10],
-[-10,  5,  5, 10, 10,  5,  5,-10],
-[-10,  0, 10, 10, 10, 10,  0,-10],
-[-10, 10, 10, 10, 10, 10, 10,-10],
-[-10,  5,  0,  0,  0,  0,  5,-10],
-[-20,-10,-10,-10,-10,-10,-10,-20]])
+bishop=np.array([-20,-10,-10,-10,-10,-10,-10,-20,
+-10,  0,  0,  0,  0,  0,  0,-10,
+-10,  0,  5, 10, 10,  5,  0,-10,
+-10,  5,  5, 10, 10,  5,  5,-10,
+-10,  0, 10, 10, 10, 10,  0,-10,
+-10, 10, 10, 10, 10, 10, 10,-10,
+-10,  5,  0,  0,  0,  0,  5,-10,
+-20,-10,-10,-10,-10,-10,-10,-20])
 bishop+=333
 
-rook=np.array([[0,  0,  0,  0,  0,  0,  0,  0],
-[5, 10, 10, 10, 10, 10, 10,  5],
-[-5,  0,  0,  0,  0,  0,  0, -5],
-[-5,  0,  0,  0,  0,  0,  0, -5],
-[-5,  0,  0,  0,  0,  0,  0, -5],
-[-5,  0,  0,  0,  0,  0,  0, -5],
-[-5,  0,  0,  0,  0,  0,  0, -5],
-[0,  0,  0,  5,  5,  0,  0,  0]])
+rook=np.array([0,  0,  0,  0,  0,  0,  0,  0,
+  5, 10, 10, 10, 10, 10, 10,  5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+ -5,  0,  0,  0,  0,  0,  0, -5,
+  0,  0,  0,  5,  5,  0,  0,  0])
 rook+=563
 
-queen=np.array([[-20,-10,-10, -5, -5,-10,-10,-20],
-[-10,  0,  0,  0,  0,  0,  0,-10],
-[-10,  0,  5,  5,  5,  5,  0,-10],
-[-5,  0,  5,  5,  5,  5,  0, -5],
-[0,  0,  5,  5,  5,  5,  0, -5],
-[-10,  5,  5,  5,  5,  5,  0,-10],
-[-10,  0,  5,  0,  0,  0,  0,-10],
-[-20,-10,-10, -5, -5,-10,-10,-20]])
+queen=np.array([-20,-10,-10, -5, -5,-10,-10,-20,
+-10,  0,  0,  0,  0,  0,  0,-10,
+-10,  0,  5,  5,  5,  5,  0,-10,
+ -5,  0,  5,  5,  5,  5,  0, -5,
+  0,  0,  5,  5,  5,  5,  0, -5,
+-10,  5,  5,  5,  5,  5,  0,-10,
+-10,  0,  5,  0,  0,  0,  0,-10,
+-20,-10,-10, -5, -5,-10,-10,-20])
 queen+=950
 
-king=np.array([[-30,-40,-40,-50,-50,-40,-40,-30],
-[-30,-40,-40,-50,-50,-40,-40,-30],
-[-30,-40,-40,-50,-50,-40,-40,-30],
-[-30,-40,-40,-50,-50,-40,-40,-30],
-[-20,-30,-30,-40,-40,-30,-30,-20],
-[-10,-20,-20,-20,-20,-20,-20,-10],
-[20, 20,  0,  0,  0,  0, 20, 20],
-[20, 30, 10,  0,  0, 10, 30, 20]])
+king=np.array([-30,-40,-40,-50,-50,-40,-40,-30,
+-30,-40,-40,-50,-50,-40,-40,-30,
+-30,-40,-40,-50,-50,-40,-40,-30,
+-30,-40,-40,-50,-50,-40,-40,-30,
+-20,-30,-30,-40,-40,-30,-30,-20,
+-10,-20,-20,-20,-20,-20,-20,-10,
+ 20, 20,  0,  0,  0,  0, 20, 20,
+ 20, 30, 10,  0,  0, 10, 30, 20])
 
 kingend=np.array([[-50,-40,-30,-20,-20,-30,-40,-50],
 [-30,-20,-10,  0,  0,-10,-20,-30],
@@ -72,116 +74,158 @@ kingend=np.array([[-50,-40,-30,-20,-20,-30,-40,-50],
 [-50,-30,-30,-30,-30,-30,-30,-50]])
 
 
+def evaluate_board(board):
+    val = 0
+    i=-1
+    state=board.fen()
+    for char in state:
+        i+=1
+        if char == " ":
+            return val
+        elif char == '/':
+            i-=1
+        elif char.isnumeric():
+            i+=int(char)-1
+            continue
+        elif char == 'p':
+            val-=pawn[i]
+        elif char == 'P':
+            val+=pawn[63-i]
+        elif char == 'n':
+            val-=knight[i]
+        elif char == 'N':
+            val+=knight[63-i]
+        elif char == 'b':
+            val-=bishop[i]
+        elif char == 'B':
+            val+=bishop[63-i]
+        elif char == 'r':
+            val-=rook[i]
+        elif char == 'R':
+            val+=rook[63-i]
+        elif char == 'q':
+            val-=queen[i]
+        elif char == 'Q':
+            val+=queen[63-i]
+        elif char == 'k':
+            val-=king[i]
+        elif char == 'K':
+            val+=king[63-i]
+
+
 def num_pieces(board):
     s=board.fen().split()[0]
     return sum(c.isalpha() for c in s)
 
-
-def evaluate(board):
-    if(num_pieces(board))<=6:
-        wdl=tablebase.probe_wdl(board)
-        turn=board.turn
-        if((turn==True and wdl==2) or (turn==False and wdl==-2)):
-            # Tablebase win
-            return 20000
-        elif(wdl==2 or wdl==-2):
-            # Tablebase loss
-            return -20000
-        else:
-            # Tablebass draw
-            return 0
-    val = 0
-    i=0
-    j=0
-    blackQueen=False
-    whiteQueen=False
-    state=board.fen()
-    for char in state:
-        if char == " ":
-            return val
-        elif char.isnumeric():
-            j+=int(char)
-            continue
-        elif char == "/":
-            i+=1
-            j=0
-        elif char == 'p':
-            val-=pawn[7-i,7-j]
-            j+=1
-        elif char == 'P':
-            val+=pawn[i,j]
-            j+=1
-        elif char == 'n':
-            val-=knight[7-i,7-j]
-            j+=1
-        elif char == 'N':
-            val+=knight[i,j]
-            j+=1
-        elif char == 'b':
-            val-=bishop[7-i,7-j]
-            j+=1
-        elif char == 'B':
-            val+=bishop[i,j]
-            j+=1
-        elif char == 'r':
-            val-=rook[7-i,7-j]
-            j+=1
-        elif char == 'R':
-            val+=rook[i,j]
-            j+=1
-        elif char == 'q':
-            blackQueen=True
-            val-=queen[7-i,7-j]
-            j+=1
-        elif char == 'Q':
-            whiteQueen=True
-            val+=queen[i,j]
-            j+=1
-        elif char == 'k':
-            blackKing=(i,j)
-            j+=1
-        elif char == 'K':
-            whiteKing=(i,j)
-            j+=1
-        else:
-            print("Unrecognized symbol in FEN input: "+char)
-    if(whiteQueen):
-        val-=king[7-blackKing[0],7-blackKing[1]]
-    else:
-        val-=kingend[7-blackKing[0],7-blackKing[1]]
-    if(blackQueen):
-        val+=king[whiteKing[0],whiteKing[1]]
-    else:
-        val+=kingend[whiteKing[0],whiteKing[1]]
+def evaluate(prevdict, prevval, move):
+#     if(num_pieces(board))<=6:
+#         wdl=tablebase.probe_wdl(board)
+#         turn=board.turn
+#         if((turn==True and wdl==2) or (turn==False and wdl==-2)):
+#             # Tablebase win
+#             return 20000
+#         elif(wdl==2 or wdl==-2):
+#             # Tablebase loss
+#             return -20000
+#         else:
+#             # Tablebase draw
+#             return 0
+    evalin=prevval
+    attack_piece=prevdict[move.from_square].symbol()
+    if move.to_square in prevdict.keys():
+        captured_piece=prevdict[move.to_square].symbol()
+        if captured_piece == 'p':
+            prevval+=pawn[63-move.to_square]
+        elif captured_piece == 'P':
+            prevval-=pawn[move.to_square]
+        elif captured_piece == 'n':
+            prevval+=knight[63-move.to_square]
+        elif captured_piece == 'N':
+            prevval-=knight[move.to_square]
+        elif captured_piece == 'b':
+            prevval+=bishop[63-move.to_square]
+        elif captured_piece == 'B':
+            prevval-=bishop[move.to_square]
+        elif captured_piece == 'r':
+            prevval+=rook[63-move.to_square]
+        elif captured_piece == 'R':
+            prevval-=rook[move.to_square]
+        elif captured_piece == 'q':
+            prevval+=queen[63-move.to_square]
+        elif captured_piece == 'Q':
+            prevval-=queen[move.to_square]
+        elif captured_piece == 'k':
+            prevval+=king[63-move.to_square]
+        elif captured_piece == 'K':
+            prevval-=king[move.to_square]
+    promote=move.promotion
+    if promote is not None:
+        if attack_piece == 'p':
+            prevval+=pawn[move.from_square]
+            if promote==2:
+                prevval-=knight[move.to_square]
+            elif promote==3:
+                prevval-=bishop[move.to_square]
+            elif promote==4:
+                prevval-=rook[move.to_square]
+            elif promote==5:
+                prevval-=queen[move.to_square]
+            else:
+                print("Promotion error")
+        elif attack_piece == 'P':
+            prevval-=pawn[63-move.from_square]
+            if promote==2:
+                prevval+=knight[63-move.to_square]
+            elif promote==3:
+                prevval+=bishop[63-move.to_square]
+            elif promote==4:
+                prevval+=rook[63-move.to_square]
+            elif promote==5:
+                prevval+=queen[63-move.to_square]
+            else:
+                print("Promotion error")
         
-        
-def sort_moves(board):
-    legal_moves=list(board.legal_moves)
-    castle=[]
-    checks=[]
-    captures=[]
-    mates=[]
-    indices=list(range(board.legal_moves.count()))
-    for i in range(len(legal_moves)):
-        legal_moves[i]=board.san(legal_moves[i])
-        if '#' in legal_moves[i]:
-            mates.append(i)
-            indices.remove(i)
-        elif '+' in legal_moves[i]:
-            checks.append(i)
-            indices.remove(i)
-        elif 'x' in legal_moves[i]:
-            captures.append(i)
-            indices.remove(i)
-        elif '-' in legal_moves[i]:
-            castle.append(i)
-            indices.remove(i)
-    random.shuffle(indices)
-    indices=mates+checks+captures+castle+indices
-    return np.take(legal_moves,indices)
+    if attack_piece == 'p':
+        prevval-=pawn[move.to_square]
+        prevval+=pawn[move.from_square]
+    elif attack_piece == 'P':
+        prevval-=pawn[63-move.from_square]
+        prevval+=pawn[63-move.to_square]
+    elif attack_piece == 'n':
+        prevval-=knight[move.to_square]
+        prevval+=knight[move.from_square]
+    elif attack_piece == 'N':
+        prevval-=knight[63-move.from_square]
+        prevval+=knight[63-move.to_square]
+    elif attack_piece == 'b':
+        prevval-=bishop[move.to_square]
+        prevval+=bishop[move.from_square]
+    elif attack_piece == 'B':
+        prevval-=bishop[63-move.from_square]
+        prevval+=bishop[63-move.to_square]
+    elif attack_piece == 'r':
+        prevval-=rook[move.to_square]
+        prevval+=rook[move.from_square]
+    elif attack_piece == 'R':
+        prevval-=rook[63-move.from_square]
+        prevval+=rook[63-move.to_square]
+    elif attack_piece == 'q':
+        prevval-=queen[move.to_square]
+        prevval+=queen[move.from_square]
+    elif attack_piece == 'Q':
+        prevval-=queen[63-move.from_square]
+        prevval+=queen[63-move.to_square]
+    elif attack_piece == 'k':
+        prevval-=king[move.to_square]
+        prevval+=king[move.from_square]
+    elif attack_piece == 'K':
+        prevval-=king[63-move.from_square]
+        prevval+=king[63-move.to_square]
+    return prevval
 
 
-def minimax(currdepth, maxdepth, board, alpha, beta):
+def minimax(currdepth, maxdepth, board, alpha, beta, prevval, move):
+    #global mtime    
     if board.outcome() is not None:
         winner=board.outcome().winner
         if winner is None:
@@ -191,14 +235,32 @@ def minimax(currdepth, maxdepth, board, alpha, beta):
         else:
             return -20000
     if currdepth==maxdepth:
-        return evaluate(board)
+        return prevval
+    
+    prevdict=board.piece_map()
+    moves=np.array(list(board.legal_moves))
+    vals=np.zeros(len(moves), dtype='int')
+    for i in range(len(moves)):
+        vals[i]=evaluate(prevdict, prevval, moves[i])
+    #ms=time.time()
+    ordering=np.argsort(vals)
+    #mtime+=time.time()-ms
+    vals=np.take(vals,ordering)
+    moves=np.take(moves,ordering)
+    
+    
+    
+    
+    
+
     if board.turn:  # white player
-        best = -40000
-        moves=sort_moves(board)  
+        moves=np.flip(moves)
+        vals=np.flip(vals)
+        best = -20000
         index_max=-1
         for i in range(len(moves)):
-            board.push_san(moves[i])
-            val=minimax(currdepth+1, maxdepth, board, alpha, beta)
+            board.push(moves[i])
+            val = minimax(currdepth+1, maxdepth, board, alpha, beta, vals[i], moves[i])
             board.pop()
             if best<val:
                 best=val
@@ -206,17 +268,17 @@ def minimax(currdepth, maxdepth, board, alpha, beta):
                 alpha=max(alpha,best)
                 if beta<=alpha:
                     break
+            
         if currdepth==0:
             return [best, moves[index_max]]
         else:
             return best
     else:  # Black player
-        best=40000
-        moves=sort_moves(board)
+        best=20000
         index_min=-1
         for i in range(len(moves)):
-            board.push_san(moves[i])
-            val=minimax(currdepth+1, maxdepth, board, alpha, beta)
+            board.push(moves[i])
+            val = minimax(currdepth+1, maxdepth, board, alpha, beta, vals[i], moves[i])
             board.pop()
             if val<best:
                 index_min=i
@@ -236,41 +298,51 @@ def flip(boardstr):
     return reverse
 
         
-def play():
+def play(depth):
     color=None
     while color!='w' and color!='b':
         color=input("Welcome to Grant's chess program!\nChoose a color.\nEnter w or b: ")
     board=chess.Board()
+    move_counter=1
     if color == 'b':
         while board.outcome() is None:
-            comp_move=minimax(0,3,board,-40000,40000)[1]
-            board.push_san(comp_move)
-            print(comp_move+"\n")
-            print(flip(chess.BaseBoard(board.board_fen()).unicode()))
+            comp_move=minimax(0,depth,board,-40000,40000, evaluate_board(board), None)[1]
+            print(str(move_counter)+". "+board.san(comp_move)+"\n")
+            board.push(comp_move)
+            print(flip(chess.BaseBoard(board.board_fen()).unicode(invert_color=True)))
             print("")
+            if (board.outcome() is not None):
+                break
             while True:
-                player_move=input("Enter a move: ")
+                player_move=input(str(move_counter)+"... ")
                 try:
                     board.push_san(player_move)
                     break
                 except:
                     print(player_move+" is not a valid move.\n")
-            print(flip(chess.BaseBoard(board.board_fen()).unicode()))
+            print(flip(chess.BaseBoard(board.board_fen()).unicode(invert_color=True)))
             print("")
+            move_counter+=1
     if color == 'w':
+        print(chess.BaseBoard(board.board_fen()).unicode(invert_color=True))
+        print("")
         while board.outcome() is None:
             while True:
-                player_move=input("Enter a move: ")
+                player_move=input(str(move_counter)+". ")
                 try:
                     board.push_san(player_move)
                     break
                 except:
                     print(player_move+" is not a valid move.\n")
-            print(chess.BaseBoard(board.board_fen()).unicode())
+            print(chess.BaseBoard(board.board_fen()).unicode(invert_color=True))
             print("")
-            comp_move=minimax(0,3,board,-40000,40000)[1]
-            board.push_san(comp_move)
-            print(comp_move+"\n")
-            print(chess.BaseBoard(board.board_fen()).unicode())
+            if(board.outcome() is not None):
+                break
+            comp_move=minimax(0,depth,board,-40000,40000, evaluate_board(board), None)[1]
+            print(str(move_counter)+"... "+board.san(comp_move)+"\n")
+            board.push(comp_move)
+            print(chess.BaseBoard(board.board_fen()).unicode(invert_color=True))
             print("")
+            move_counter+=1
+    print(board.outcome())
             
