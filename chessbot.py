9d2124589e614f33615e4924b957cc840e385441
Grant Jensen <gjensen6@wisc.edu>
2021-08-14 18:29:54 -0500

    Added comments, did some small optimizations. Incentivized castling, moving king to center of board

--- chessbot.py
+++ chessbot.py
@@ -66,17 +66,26 @@ king=np.array([-30,-40,-40,-50,-50,-40,-40,-30,
  20, 30, 10,  0,  0, 10, 30, 20])
 king+=20000
 
-kingend=np.array([[-50,-40,-30,-20,-20,-30,-40,-50],
-[-30,-20,-10,  0,  0,-10,-20,-30],
-[-30,-10, 20, 30, 30, 20,-10,-30],
-[-30,-10, 30, 40, 40, 30,-10,-30],
-[-30,-10, 30, 40, 40, 30,-10,-30],
-[-30,-10, 20, 30, 30, 20,-10,-30],
-[-30,-30,  0,  0,  0,  0,-30,-30],
-[-50,-30,-30,-30,-30,-30,-30,-50]])
-
+#King endgame position values
+kingend=np.array([-50,-40,-30,-20,-20,-30,-40,-50,
+-30,-20,-10,  0,  0,-10,-20,-30,
+-30,-10, 20, 30, 30, 20,-10,-30,
+-30,-10, 30, 40, 40, 30,-10,-30,
+-30,-10, 30, 40, 40, 30,-10,-30,
+-30,-10, 20, 30, 30, 20,-10,-30,
+-30,-30,  0,  0,  0,  0,-30,-30,
+-50,-30,-30,-30,-30,-30,-30,-50])
+kingend+=20000
+# Above are the values given for each piece at each square. Bottom left of array represents the a1 square, top right is h8.
+# Note that these are reflected for black evaluations.
 
 def evaluate_board(board):
+    """
+    Args:
+        board: chess.Board object (chess-python package)
+    Output:
+        val: Integer evaluation in centipawn units. Positive if the player to move is in a winning position.
+    """
     val = 0
     i=-1
     state=board.fen()
@@ -119,10 +128,27 @@ def evaluate_board(board):
 
 
 def evaluate(prevdict, prevval, move, board):
+    """
+    Different from evaluate_board in that it updates the previous evaluation from the next move, as opposed
+    to calculating the evaluation from scratch like in evaluate_board.
+    Args:
+        prevdict: dictionary object before move is played (see updatePieceMap) maps squares to pieces
+        prevval: previous evaluation of the position
+        move: move to be played (chess.Move object)
+        board: chess.Board object, board state before move is played.
+    Output:
+        prevval: Evaluation of position after move is played.
+    """
+    global whiteQueen
+    global blackQueen
     who_to_move = not board.turn
     if who_to_move:
         prevval=-prevval
     if board.is_castling(move):
+        if board.turn: #Incentivize castling
+            prevval+=50
+        else:
+            prevval-=50
         if move.to_square==6:#white kingside castle
             prevval-=king[4]
             prevval+=king[6]
@@ -176,8 +202,10 @@ def evaluate(prevdict, prevval, move, board):
         elif captured_piece == 'R':
             prevval-=rook[63-move.to_square]
         elif captured_piece == 'q':
+            blackQueen=False
             prevval+=queen[move.to_square]
         elif captured_piece == 'Q':
+            whiteQueen=False
             prevval-=queen[63-move.to_square]
         elif captured_piece == 'k':
             prevval+=king[move.to_square]
@@ -241,17 +269,32 @@ def evaluate(prevdict, prevval, move, board):
         prevval-=queen[63-move.from_square]
         prevval+=queen[63-move.to_square]
     elif attack_piece == 'k':
-        prevval-=king[move.to_square]
-        prevval+=king[move.from_square]
+        if whiteQueen:
+            prevval-=king[move.to_square]
+            prevval+=king[move.from_square]
+        else:
+            prevval-=kingend[move.to_square]
+            prevval+=kingend[move.from_square]
     elif attack_piece == 'K':
-        prevval-=king[63-move.from_square]
-        prevval+=king[63-move.to_square]
+        if blackQueen:
+            prevval-=king[63-move.from_square]
+            prevval+=king[63-move.to_square]
+        else:
+            prevval-=kingend[63-move.from_square]
+            prevval+=kingend[63-move.to_square]
     if who_to_move:#white
         return prevval
     else:
         return -prevval
 
+
 def updatePieceMap(piece_map, move, board):
+    """Updates a piece_map given a move. 
+    Args:
+        piece_map: Same type as board.piece_map(), update manually after each move instead of recalling it (performance increase). 
+        move: chess.Move to be played
+        board: chess.Board object (used for checking if castling, en passant occurs
+    """
     if move==None:
         return piece_map
     promote=move.promotion
@@ -295,6 +338,17 @@ def updatePieceMap(piece_map, move, board):
     
 hasharray=chess.polyglot.POLYGLOT_RANDOM_ARRAY
 def updateHash(board, move, piece_map, prevhash):
+    """
+    Zobrist hashing of the position. Note XOR is its own inverse operation, therefore we can update the hash by XOR-ing in the move to be played as opposed to re-hashing the entire position. This hash is used for transposition tables
+    Args:
+        board: chess.Board object of board state before move is played
+        move: chess.Move to be played
+        piece_map: Dictionary mapping square numbers to pieces
+        prevhash: Value of hash before move is played
+    Output:
+        prevhash: Value of hash after move is played
+    """
+        
     if move is None:
         return prevhash
     prevhash^=hasharray[780]#adjust for move changing
@@ -336,13 +390,28 @@ def updateHash(board, move, piece_map, prevhash):
 
 
 def minimax(depth, board, alpha, beta, prevval, move, piece_map, start_time, time_to_run, prevhash):
+    """
+    This is technically a Principal Variation search. Main driver of the program.
+    Args:
+        depth: Depth of search to be performed.
+        board: chess.Board() object of board state
+        alpha: See minimax psuedo code for information on alpha-beta pruning
+        beta: ^^
+        prevval: Previous evaluation of position. Use for evalute()
+        piece_map: Map of squares to pieces. Used in updatePieceMap()
+        start_time: time when board evaluation first started (Iterative Deepening)
+        time_to_run: Amount of time we want to look for a move to make
+        prevhash: Previous hash of position. Used in updateHash()
+    Outputs:
+        eval: Evaluation of position
+        best_move: Best move in position
+        completed: Whether search completed for requested depth. Used for iterative deepening
+    """
     currzobrist=updateHash(board, move, piece_map, prevhash)
     if depth!=0:
         piece_map=updatePieceMap(piece_map.copy(), move, board)
     if move is not None:
         board.push(move)
-    if depth==0:
-        return [prevval, None, False]
     hashval=currzobrist%0xFFFF
     if TT[hashval] is not None:
         if TT[hashval].zobrist==currzobrist and TT[hashval].depth>=depth:
@@ -377,35 +446,51 @@ def minimax(depth, board, alpha, beta, prevval, move, piece_map, start_time, tim
     pmc=piece_map.copy()
     for i, move in enumerate(board.legal_moves):
         tmpval=evaluate(pmc,prevval,move, board)
-        if i>0:
-            currval=-minimax(depth-1, board, -alpha-1, -alpha, tmpval,move, pmc, start_time, time_to_run, currzobrist)[0]
-            if alpha<currval and currval < beta:
-                board.pop()
-                currval=-minimax(depth-1,board, -beta,-currval,tmpval,move,pmc, start_time, time_to_run, currzobrist)[0]
+        if depth==1:
+            currval=-tmpval
         else:
-            currval=-minimax(depth-1,board,-beta,-alpha,tmpval,move,pmc, start_time, time_to_run, currzobrist)[0]
-        board.pop()
+            if i>0:
+                currval=-minimax(depth-1, board, -alpha-1, -alpha, tmpval,move, pmc, start_time, time_to_run, currzobrist)[0]
+                if alpha<currval and currval < beta:
+                    board.pop()
+                    currval=-minimax(depth-1,board, -beta,-currval,tmpval,move,pmc, start_time, time_to_run, currzobrist)[0]
+            else:
+                currval=-minimax(depth-1,board,-beta,-alpha,tmpval,move,pmc, start_time, time_to_run, currzobrist)[0]
+            board.pop()
         if(time.time()-start_time>=time_to_run): 
             completed=False
             break
-        if(bestval<currval):
+        if(bestval<currval): #New best move
             bestval=currval
             best_move=move
             if bestval>=beta:
-                break
+                break #Alpha beta pruning
         if alpha<bestval:
             alpha=bestval
     if(completed and best_move is not None):
         enterTT(board, origalpha, beta, -bestval, best_move, depth, piece_map, prevhash)
     return [bestval, best_move, completed]
 
-
+TT=[None]*0xFFFF
+blackQueen=False
+whiteQueen=False
 def choose_move(board, time_to_run):
+    """
+    Thin wrapper of minimax() for iterative deepening. Ie, want to increase depth until we reach time limit
+    Args:
+        board: chess.Board object to find best position in
+        time_to_run: time to look for best_move
+    Output:
+        best_move: best_move in position inputed
+    """
     start_time=time.time()
+    TT=[None]*0xFFFF
     val=evaluate_board(board)
-    for depth in range(1,10):
-        out=minimax(depth, board, -40000, 40000, val, None, board.piece_map(), start_time, time_to_run, zhash(board))
-        if out[2]:
+    piece_map=board.piece_map()
+    prevhash=zhash(board)
+    for depth in range(1,100):
+        out=minimax(depth, board, -40000, 40000, val, None, piece_map.copy(), start_time, time_to_run, prevhash)
+        if out[2]: #If completed depth before time limit
             best_move=out[1]
         if(time.time()-start_time>=time_to_run):
             break
@@ -414,6 +499,20 @@ def choose_move(board, time_to_run):
 
 
 def enterTT(board, alpha, beta, val, best_move, depth, piece_map, prevhash):
+    """
+    Entering positions into the Transposition Table (TT).
+    Args:
+        board: board state to hash and put into table (prior to move)
+        alpha: See minimax()
+        beta: See minimax()
+        val: Valuation of position
+        best_move: Move to be made
+        depth: Depth the position was found at
+        piece_map: Mapping of squares to pieces, used in updateHash (prior to move)
+        prevhash: previous hash of board 
+    Ouput:
+        None, but we do enter values into TT. 
+    """
     zobrist=updateHash(board,best_move,piece_map, prevhash)
     hashval=zobrist%0xFFFF
     if(val<=alpha):
@@ -434,13 +533,26 @@ def enterTT(board, alpha, beta, val, best_move, depth, piece_map, prevhash):
     
 
 def flip(boardstr):
+    """Flips board for printing when player is black
+    Args:
+        boardstr: unicode format of board state
+    Ouput:
+        reverse: unicode of mirrored board state
+    """
     l = boardstr.split("\n")
     l = [ll[::-1] for ll in l]
     reverse = "\n".join(l[::-1])
     return reverse
 
-TT=[None]*0xFFFF
+        
 def play(time_to_run):
+    """
+    If you wish to play against the bot in terminal, this is where that occurs.
+    Args:
+        time_to_run: Time you want the bot to think per move.
+    Output:
+        None
+    """
     color=None
     while color!='w' and color!='b':
         color=input("Welcome to Grant's chess program!\nChoose a color.\nEnter w or b: ")
@@ -490,6 +602,9 @@ def play(time_to_run):
             
         
 class TTEntry:
+    """
+    Format of Transposition Table Entries
+    """
     def __init__(self, zobrist, depth, val, alphaflag, betaflag, exact, best_move):
         self.zobrist = zobrist #Full hash
         self.depth = depth
